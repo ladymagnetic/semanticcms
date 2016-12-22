e93e0bfe56a655ab80daa554f59d7178260252f8
gitJonHaw <jon.koenig@oth-aw.de>
2016-12-22 13:32:15 +0100

    #16 Usermanagement foreach --> while

--- Code/Usermanagement.php
+++ Code/Usermanagement.php
@@ -18,11 +18,11 @@ $dbUser = new DbUser($config['cms_db']['dbhost'], $config['cms_db']['dbuser'], $
 // actions dbuser
 if (isset($_POST['deban'])) {
     $userId = $_POST['userId'];
-    $dbUser.DebanUser($userId);    
+    $dbUser->DebanUser($userId);    
 }
 else if (isset($_POST['ban'])) {
     $userId = $_POST['userId'];
-    $dbUser.BanUser($userId);
+    $dbUser->BanUser($userId);
 }
 else if (isset($_POST['details'])) {
     $userId = $_POST['userId'];
@@ -32,16 +32,15 @@ else if (isset($_POST['details'])) {
 }
 else if (isset($_POST['delete'])) {
     $userId = $_POST['userId'];
-    $dbUser.DeleteUser($userId);
+    $dbUser->DeleteUser($userId);
 }
 else if (isset($_POST['newUser'])) {
-    $userId = dbUser.CreateUser();
-    EditUser($userId);
+    CreateNewUser();
     // has to return because other page
     return;
 }
 else if (isset($_POST['newRole'])) {
-    $roleId = $dbUser.NewRole();   
+    $roleId = $dbUser->NewRole();   
     EditRole($roleId);
     // has to return because other page
     return;
@@ -49,15 +48,15 @@ else if (isset($_POST['newRole'])) {
 else if (isset($_POST['assignRole'])) {
     $roleId = $_POST['assignedRole'];
     $userId = $_POST['userId'];
-    $dbUser.AssignRole($roleId, $userId);
+    $dbUser->AssignRole($roleId, $userId);
 }
 else if (isset($_POST['deleteRole'])) {
     $roleId = $_POST['roleId'];
-    $dbUser.DeleteRole($roleId);
+    $dbUser->DeleteRole($roleId);
 }
 else if (isset($_POST['roleDetails'])) {
     $roleId = $_POST['roleId'];
-    $dbUser.EditRole($roleId);
+    $dbUser->EditRole($roleId);
     // has to return because other page
     return;
 }
@@ -68,7 +67,7 @@ else if (isset($_POST['applyChanges']))
     $name = $_POST['name'];
     $foreName = $_POST['foreName'];
     $email = $_POST['email'];
-    $dbUser.UpdateUserDifferentNamesById($userId, $userName, $name, $foreName, $email);
+    $dbUser->UpdateUserDifferentNamesById($userId, $userName, $name, $foreName, $email);
 }
 else if (isset($_POST['applyPasswordChanges']))
 {
@@ -76,9 +75,21 @@ else if (isset($_POST['applyPasswordChanges']))
     $password = $_POST['currentPassword'];
     $newPassword = $_POST['newPassword'];
     $newPasswordRepeat = $_POST['newPasswordRepeat'];
-    $dbUser.ApplyPasswordChangesToUser($userId, $password, $newPassword, $newPasswordRepeat);
+    $dbUser->ApplyPasswordChangesToUser($userId, $password, $newPassword, $newPasswordRepeat);
 }
-else if (isset($_POST['saveRoleChanges'])) {
+else if (isset($_POST['registrateUser']))
+{
+    $role_id = $_POST['role'];
+    $lastname = $_POST['name'];
+    $firstname = $_POST['foreName'];
+    $username = $_POST['userName'];
+    $password = $_POST['currentPassword'];
+    $email = $_POST['email'];
+    $birthdate = $_POST['birthdate'];
+    $dbUser->RegistrateUser($role_id, $lastname, $firstname, $username, $password, $email, $birthdate);
+}
+else if (isset($_POST['saveRoleChanges'])) 
+{
     $roleId = $_POST['roleId'];
     $rolename = $_POST['rolename'];
     $guestbookmanagement = $_POST['guestbookmanagement'];
@@ -87,13 +98,13 @@ else if (isset($_POST['saveRoleChanges'])) {
     $articlemanagement = $_POST['articlemanagement'];
     $guestbookusage = $_POST['guestbookusage'];
     $templateconstruction = $_POST['templateconstruction'];
-    $dbUser.UpdateRoleById($roleId, $rolename, $guestbookmanagement, $usermanagement, $pagemanagement, $articlemanagement, $guestbookusage, $templateconstruction);
+    $dbUser->UpdateRoleById($roleId, $rolename, $guestbookmanagement, $usermanagement, $pagemanagement, $articlemanagement, $guestbookusage, $templateconstruction);
 }
 
-BackendComponentPrinter::printHead("Benutzerverwaltung");
+BackendComponentPrinter::PrintHead("Benutzerverwaltung");
 /* menue */
 /* dynamisch erzeugt je nach Rechten */
-BackendComponentPrinter::printSidebar(array());// printSidebar($_SESSION["permissions"]);
+BackendComponentPrinter::PrintSidebar(array());// PrintSidebar($_SESSION["permissions"]);
 
 echo
 "<section id='main'>
@@ -106,38 +117,40 @@ echo
             <th>Aktion</th>
         </tr>";
             // foreach user in database print
-            $userRows = $dbUser.SelectAllUsers();
-            $roleRows = $dbUser.SelectAllRoles();
-            foreach ($userRows as $row) {
+            $row = $dbUser->SelectAllUsers();
+            while ( $row != null)
 
                 echo 
                 "<tr>".$row['firstname']." ".$row['lastname']."<td>";
 
                 // if user is banned/debanned
-                $debanned = $dbUser.IsUserBanned($row['id']);
+                $debanned = $dbUser->IsUserBanned($row['id']);
                 if ($debanned)
                 {
                     echo "<td>
                     <form method='post' action='Usermanagement.php'>
-                    <input id='deban' name='deban' type='button' value='entsperren'>";
+                    <input id='deban' name='deban' type='submit' value='entsperren'>";
                 }
                 else
                 {
                     echo "<td>
                     <form method='post' action='Usermanagement.php'>
-                    <input id='ban' name='ban' type='button' value='sperren'>";
+                    <input id='ban' name='ban' type='submit' value='sperren'>";
                 }
                 echo
                     "<input id='userId' name='userId' type='hidden' value='".$row['id']."'></form></td>";
                 echo 
                     "<td><form action='../lib/BackendComponentPrinter.class.php'> <label>Rolle: <select name='assignedRole'>";
-                    foreach ($roleRows as $rolerow) {
+                    $rolerow = $dbUser->SelectAllRoles();
+                    while ( $row != null)
+                    {
                         echo "<option id='".row['id']."'";
                         if ($rolerow['id'] == $row['role'])
                         {
                             echo " selected ";
                         }
-                        echo ">".$rolerow['rolename']."</option>"; 
+                        echo ">".$rolerow['rolename']."</option>";
+                        $rolerow = $dbUser->SelectAllRoles();
                     }
                 echo
                     "<input id='userId' name='userId' type='hidden' value='".$row['id']."'></select></label></form></td>";
@@ -145,16 +158,17 @@ echo
                     "<td>".$row['role_id']."</td>";
                 echo
                     "<td><form method='post' action='Usermanagement.php'>"
-                    ."<input id='details' name='details' type='button' value='Details'><input id='delete' name='delete' type='button' value='löschen'>"
+                    ."<input id='details' name='details' type='submit' value='Details'><input id='delete' name='delete' type='submit' value='löschen'>"
                     ."<input id='userId' name='userId' type='hidden' value='".$row['id']."'>" 
                     ."</form></td>";
                 echo 
                     "</tr>";
+                $row = $dbUser->SelectAllUsers();
             }
 echo
     "</table>
     <form method='post' action='Usermanagement.php'>
-        <input id='newUser' name='newUser' type='button' value='Neuer Benutzer'>
+        <input id='newUser' name='newUser' type='submit' value='Neuer Benutzer'>
         <input id='userId' name='userId' type='hidden' value='userId'>
     </form>
     <h2>Rollen definieren</h2>
@@ -164,15 +178,17 @@ echo
             <th>Aktion</th>
         </tr>";
             // foreach role in database print
-            $roleRows = $dbUser.SelectAllRoles();
-            foreach ($roleRows as $row) {
+            $row = $dbUser->SelectAllRoles();
+            while ($row != null)
+            {
                 echo "<tr>";
                 echo "<td>";
                 echo $row['rolename'];
                 echo "</td>";
-                echo "<td><form method='post' action='Usermanagement.php'><input id='roleDetails' name='roleDetails' type='button' value='Details'><input id='deleteRole' name='deleteRole' type='button' value='Rolle löschen'></form></td>";
+                echo "<td><form method='post' action='Usermanagement.php'><input id='roleDetails' name='roleDetails' type='submit' value='Details'><input id='deleteRole' name='deleteRole' type='submit' value='Rolle löschen'></form></td>";
                 echo "<input id='roleId' name='roleId' type='hidden' value='".$row['id']."'>";
                 echo "</tr>";
+                $row = $dbUser->SelectAllRoles();
             }
 echo
     "</section>
@@ -183,18 +199,16 @@ echo
 
 function EditUser($userId)
 {
-printHead("Benutzerverwaltung");
-
+BackendComponentPrinter::PrintHead("Benutzerverwaltung");
 /* menue */
 /* dynamisch erzeugt je nach Rechten */
-require_once 'lib/BackendComponentPrinter.class.php';
-
-BackendComponentPrinter::printSidebar($config['cms_db']['dbuser']);
+BackendComponentPrinter::PrintSidebar(array());// PrintSidebar($_SESSION["permissions"]);
     echo
     "<section id='main'>
     <h1>Kontodaten bearbeiten</h1>
         <form method='post' action='../lib/BackendComponentPrinter.class.php'>";
-    $userRow = $dbUser.SelectUserById($userId);
+    $dbUser = new DbUser();
+    $userRow = $dbUser->SelectUserById($userId);
     echo
             "<label for='userName'>Benutzername</label>
             <input id='userName' name='userName' type='text' value='".$userRow['username']."'>";
@@ -209,7 +223,7 @@ BackendComponentPrinter::printSidebar($config['cms_db']['dbuser']);
             <input id='email' name='email' type='text' value='".$userRow['email']."'>";
     echo
             "<input id='userId' name='userId' type='hidden' value='".$userId."'>".
-            "<input id='applyChanges' name='applyChanges' type='button' value='Änderungen übernehmen'>";
+            "<input id='applyChanges' name='applyChanges' type='submit' value='Änderungen übernehmen'>";
         echo
         "</form>
         <h2>Passwort ändern</h2>
@@ -222,7 +236,7 @@ BackendComponentPrinter::printSidebar($config['cms_db']['dbuser']);
             <label for='newPasswordRepeat'>neues Passwort bestätigen</label>
             <input id='newPasswordRepeat' name='newPasswordRepeat' type='password'>
             <input id='userId' name='userId' type='hidden' value='".$userId."'>".
-            "<input id='applyPasswordChanges' name='applyPasswordChanges' type='button' value='Passwort übernehmen'>";
+            "<input id='applyPasswordChanges' name='applyPasswordChanges' type='submit' value='Passwort übernehmen'>";
         echo
         "</form>
     </section>
@@ -232,18 +246,17 @@ BackendComponentPrinter::printSidebar($config['cms_db']['dbuser']);
 }
 function EditRole($roleId)
 {
-printHead("Benutzerverwaltung");
 
+BackendComponentPrinter::PrintHead("Benutzerverwaltung");
 /* menue */
 /* dynamisch erzeugt je nach Rechten */
-require_once 'lib/BackendComponentPrinter.class.php';
-
-BackendComponentPrinter::printSidebar($config['cms_db']['dbuser']);
+BackendComponentPrinter::PrintSidebar(array());// PrintSidebar($_SESSION["permissions"]);
     
     echo
     "<section id='main'>
     <h1>Rolle bearbeiten</h1>";
-    $roleRow = $dbUser.SelectRoleById($roleId);
+    $dbUser = new DbUser();
+    $roleRow = $dbUser->SelectRoleById($roleId);
     echo
             "<form method='post' action='Usermanagement.php'>".
             "<input id='userId' name='userId' type='hidden' value='".$roleId."'>".
@@ -270,6 +283,50 @@ BackendComponentPrinter::printSidebar($config['cms_db']['dbuser']);
     echo
             "<label for='templateconstruction'>Email</label>".
             "<input id='templateconstruction' name='templateconstruction' type='checkbox' value='".$roleRow['templateconstruction']."'>".
-            "<input id='saveRoleChanges' name='saveRoleChanges' type='button' value'Rollenänderung speichern'></form>";
+            "<input id='saveRoleChanges' name='saveRoleChanges' type='submit' value'Rollenänderung speichern'></form>";
+}
+function CreateNewUser()
+{
+BackendComponentPrinter::PrintHead("Benutzerverwaltung");
+/* menue */
+/* dynamisch erzeugt je nach Rechten */
+BackendComponentPrinter::PrintSidebar(array());// PrintSidebar($_SESSION["permissions"]);
+    echo
+    "<section id='main'>
+    <h1>Neuer Benutzer</h1>
+        <form method='post' action='../lib/BackendComponentPrinter.class.php'>";
+    echo
+            "<label for='userName'>Benutzername</label>
+            <input id='userName' name='userName' type='text'>";
+    echo
+            "<label for='name'>Name</label>
+            <input id='name' name='name' type='text'>";
+    echo    
+            "<label for='foreName'>Vorname</label>
+            <input id='foreName' name='foreName' type='text'>";
+    echo
+            "<label for='email'>Email</label>
+            <input id='email' name='email' type='text'>";
+            "<label for='currentPassword'>aktuelles Passwort</label>
+            <input id='currentPassword' name='currentPassword' type='password'>";
+    $dbUser = new DbUser();
+    echo
+            "<label>Rolle: <select name='role'>";
+    $rolerow = $dbUser->SelectAllRoles();
+    while ($rolerow != null)
+    {
+        echo "<option id='".row['id']."'";
+        echo ">".$rolerow['rolename']."</option>"; 
+        $rolerow = $dbUser->SelectAllRoles();
+    }
+    echo
+            "</select></label>";
+    echo
+            "<input type='date' name='birthdate' id='birthdate'>";
+    echo
+            "<input id='registrateUser' name='registrateUser' type='submit' value='Anwender erstellen'>";
+
+    echo
+        "</form></section></body></html>";
 }
 ?>
