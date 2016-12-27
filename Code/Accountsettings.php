36e3565df966917a39c0ed179861f66559b5d98b
gitJonHaw <jon.koenig@oth-aw.de>
2016-12-27 18:38:13 +0100

    #16 Benutzer oder Email existiert bereits
    Wenn der Benutzer oder die Email bereits existiert, wird eine Meldung
    angezeigt. Beides gleichzeitig ist jetz nicht mit dabei, aber dann kommt
    es halt hintereinander.

--- Code/Accountsettings.php
+++ Code/Accountsettings.php
@@ -27,7 +27,40 @@ if (isset($_POST['applyChanges']))
     $name = $_POST['name'];
     $foreName = $_POST['foreName'];
     $email = $_POST['email'];
-    $dbUser->UpdateUserDifferentNamesById($name, $foreName, $userName, $email, $userId);
+    if ($dbUser->EmailAlreadyExists($email))
+    {
+        echo
+        /* Bootstrap temporary for nice alert */
+            "<!-- Latest compiled and minified CSS -->
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
+            <!-- Optional theme -->
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css' integrity='sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp' crossorigin='anonymous'>
+            <!-- Latest compiled and minified JavaScript -->
+            <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>";
+        echo
+        "<div class='alert alert-danger warning'>
+        <strong>Warnung!</strong> Email existiert bereits.
+        </div";
+    }
+    else if ($dbUser->UsernameAlreadyExists($username))
+    {
+        echo
+        /* Bootstrap temporary for nice alert */
+            "<!-- Latest compiled and minified CSS -->
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
+            <!-- Optional theme -->
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css' integrity='sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp' crossorigin='anonymous'>
+            <!-- Latest compiled and minified JavaScript -->
+            <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>";
+        echo
+        "<div class='alert alert-danger warning'>
+        <strong>Warnung!</strong> Benutzername existiert bereits.
+        </div";
+    }
+    else
+    {
+        $dbUser->UpdateUserDifferentNamesById($name, $foreName, $userName, $email, $userId);
+    }
 }
 // if submit button with name 'applyPasswordChanges' is pressed
 else if (isset($_POST['applyPasswordChanges']))
--- Code/Usermanagement.php
+++ Code/Usermanagement.php
@@ -101,7 +101,44 @@ else if (isset($_POST['registrateUser']))
     $password = $_POST['currentPassword'];
     $email = $_POST['email'];
     $birthdate = $_POST['birthdate'];
-    $dbUser->RegistrateUser($role_id, $lastname, $firstname, $username, $password, $email, $birthdate);
+    if ($dbUser->EmailAlreadyExists($email))
+    {
+        echo
+        /* Bootstrap temporary for nice alert */
+            "<!-- Latest compiled and minified CSS -->
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
+            <!-- Optional theme -->
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css' integrity='sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp' crossorigin='anonymous'>
+            <!-- Latest compiled and minified JavaScript -->
+            <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>";
+        CreateNewUser($dbUser);
+        echo
+        "<div class='alert alert-danger warning'>
+        <strong>Warnung!</strong> Email existiert bereits.
+        </div";
+        return;
+    }
+    else if ($dbUser->UsernameAlreadyExists($username))
+    {
+        echo
+        /* Bootstrap temporary for nice alert */
+            "<!-- Latest compiled and minified CSS -->
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>
+            <!-- Optional theme -->
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css' integrity='sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp' crossorigin='anonymous'>
+            <!-- Latest compiled and minified JavaScript -->
+            <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa' crossorigin='anonymous'></script>";
+        CreateNewUser($dbUser);
+        echo
+        "<div class='alert alert-danger warning'>
+        <strong>Warnung!</strong> Benutzername existiert bereits.
+        </div";
+        return;
+    }
+    else
+    {
+        $dbUser->RegistrateUser($role_id, $lastname, $firstname, $username, $password, $email, $birthdate);
+    }
 }
 // if submit button with name 'saveRoleChanges' is pressed
 else if (isset($_POST['saveRoleChanges'])) 
